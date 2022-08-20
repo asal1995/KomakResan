deaa3fa6e4fb1c521157c8ca7dff9b4aa9bc6696
Asal <asalhnzd.1995@gmail.com>
2022-08-20 15:06:20 +0430

    Create User Data Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    USER_TYPE = [
+        ('normal', 'normal'),
+        ('silver', 'silver'),
+        ('gold', 'gold'),
+    ]
+    username = models.CharField(max_length=45, unique=True)
+    email = models.EmailField('email address', unique=True)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE, null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
