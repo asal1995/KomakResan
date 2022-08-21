b8bfadf75fdfd81fef0a17c4fa42accb52f9908c
Asal <asalhnzd.1995@gmail.com>
2022-08-21 12:23:21 +0430

    Implementation reset password  Service

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,9 @@
 
-
 from rest_framework import routers
 
 from users.api import RegisterView, VerifyEmail
 
-router = routers.DefaultRouter
 router = routers.DefaultRouter()
 router.register(r'register', RegisterView, 'register'),
-router.register(r'register', VerifyEmail, 'register')
-
+router.register(r'verify', VerifyEmail, 'verify')
 
